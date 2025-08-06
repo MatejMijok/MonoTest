aa0288aeb5e6e59b60f4f861393ca44b9806489d
Matej Mijok <matej.mijok@gmail.com>
2025-08-06 14:57:52 +0200

    Implement sorting, pagination and filtering on combined view page

--- MonoTest/Controllers/HomeController.cs
+++ MonoTest/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using MonoTest.Services.Interfaces;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,13 +20,81 @@ namespace MonoTest.Controllers
             _vehicleService = vehicleService;
             _mapper = mapper;
         }
-        public async Task<ActionResult> Index(string sortOrder)
+        public async Task<ActionResult> Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
-            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.MakeSortParam = sortOrder == "make" ? "make_desc" : "make";
+            ViewBag.ModelNameSortParam = sortOrder == "model_name" ? "model_name_desc" : "model_name";
+            ViewBag.ModelAbrvSortParam = sortOrder == "model_abrv" ? "model_abrv_desc" : "model_abrv";
+            ViewBag.AbrvSortParam = sortOrder == "abrv" ? "abrv_desc" : "abrv";
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.CurrentFilter = searchString ?? currentFilter;
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
 
             var vehicleOverviewViewModel = await _vehicleService.GetVehicleOverviewAsync();
 
-            return View(vehicleOverviewViewModel);
+            if (!String.IsNullOrEmpty(searchString)) 
+            { 
+                vehicleOverviewViewModel = vehicleOverviewViewModel
+                    .Where(v => v.Name.ToLower().Contains(searchString.ToLower())
+                             || v.MakeName.ToLower().Contains(searchString.ToLower())
+                             || v.Abrv.ToLower().Contains(searchString.ToLower())
+                             || v.MakeAbrv.ToLower().Contains(searchString.ToLower()))
+                    .ToList();
+            }
+
+            switch (sortOrder)
+            {
+                case "make_desc":
+                    vehicleOverviewViewModel = vehicleOverviewViewModel
+                        .OrderByDescending(v => v.MakeName).ToList();
+                    break;
+                case "make":
+                    vehicleOverviewViewModel = vehicleOverviewViewModel
+                        .OrderBy(v => v.MakeName).ToList();
+                    break;
+                case "model_name_desc":
+                    vehicleOverviewViewModel = vehicleOverviewViewModel
+                        .OrderByDescending(v => v.Name).ToList();
+                    break;
+                case "model_name":
+                    vehicleOverviewViewModel = vehicleOverviewViewModel
+                        .OrderBy(v => v.Name).ToList();
+                    break;
+                case "model_abrv_desc":
+                    vehicleOverviewViewModel = vehicleOverviewViewModel
+                        .OrderByDescending(v => v.Abrv).ToList();
+                    break;
+                case "model_abrv":
+                    vehicleOverviewViewModel = vehicleOverviewViewModel
+                        .OrderBy(v => v.Abrv).ToList();
+                    break;
+                case "abrv_desc":
+                    vehicleOverviewViewModel = vehicleOverviewViewModel
+                        .OrderByDescending(v => v.MakeAbrv).ToList();
+                    break;
+                case "abrv":
+                    vehicleOverviewViewModel = vehicleOverviewViewModel
+                        .OrderBy(v => v.MakeAbrv).ToList();
+                    break;
+                default:
+                    vehicleOverviewViewModel = vehicleOverviewViewModel
+                        .OrderBy(v => v.MakeName).ToList();
+                    break;
+            }
+
+
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+
+            return View(vehicleOverviewViewModel.ToPagedList(pageNumber, pageSize));
         }
 
         public ActionResult About()
