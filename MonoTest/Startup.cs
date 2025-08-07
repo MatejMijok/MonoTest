dfc568ac0f218f4f51e49dbbe6b7f5693955d93e
Matej Mijok <matej.mijok@gmail.com>
2025-08-07 12:28:18 +0200

    Add missing authentication files

---
+++ MonoTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MonoTest.Startup))]
+namespace MonoTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
